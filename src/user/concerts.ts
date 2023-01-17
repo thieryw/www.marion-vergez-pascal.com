c64c82ed5f8f4e288842cc6b23072eaa910e6ee9
thieryw <williamalexthiery@hotmail.fr>
2023-01-17 18:56:32 +0100

    update concerts

--- src/user/concerts.ts
+++ src/user/concerts.ts
@@ -13,6 +13,22 @@ export const concerts: {
 
 		{
 			"fr": {
+				"date": "Jeudi 2 Février 20 h 00",
+				"title": "Cathédrale St Louis des Invalides",
+				"description": `Concert du gouverneur militaire de Paris.
+
+Mezzo-Soprano solo du Dixit Dominus Haendel.
+
+Ensemble vocal de l’Ecole polytechnique.
+Orchestre des Troupes de Marine (dir. Grégoire Michaud)
+
+				`
+
+			},
+			"linkHref": "https://www.helloasso.com/associations/association-pour-le-developpement-des-uvres-d-entraide-dans-l-armee-ado/evenements/concert-du-gouverneur-militaire-de-paris-2-fevrier-2023"
+		},
+		{
+			"fr": {
 				"date": "7 Mars 14h30, les 8 Mars 10 Mars & 11 Mars à 20h",
 				"title": "Didon et Enée de Purcell. Rôle de l’Enchanteresse",
 				"description": `Direction : Leonardo Garcia Alarcón;
@@ -59,22 +75,6 @@ Collectif Io
 
 			},
 			
-		},
-		{
-			"fr": {
-				"date": "Jeudi 2 Février 20 h 00",
-				"title": "Cathédrale St Louis des Invalides",
-				"description": `Concert du gouverneur militaire de Paris.
-
-Mezzo-Soprano solo du Dixit Dominus Haendel.
-
-Ensemble vocal de l’Ecole polytechnique.
-Orchestre des Troupes de Marine (dir. Grégoire Michaud)
-
-				`
-
-			},
-			"linkHref": "https://www.helloasso.com/associations/association-pour-le-developpement-des-uvres-d-entraide-dans-l-armee-ado/evenements/concert-du-gouverneur-militaire-de-paris-2-fevrier-2023"
 		},
 		{
 			"fr": {
