627dc1fd05f13c215a20aeef99e5d64be8d0f4cf
thieryw <39378411+thieryw@users.noreply.github.com>
2026-07-01 20:09:05 +0000

    update images

--- src/generatedImgExports.ts
+++ src/generatedImgExports.ts
@@ -1,270 +1,325 @@
-import _0 from "./assets/img/media/0.69-.jpg";
-import _1 from "./assets/img/media/0.70-.jpg";
-import _2 from "./assets/img/media/0.71-.jpg";
-import _3 from "./assets/img/media/0.72-.jpg";
-import _4 from "./assets/img/media/0.73-.jpg";
-import _5 from "./assets/img/media/0.74-.jpg";
-import _6 from "./assets/img/media/0.75-.jpg";
-import _7 from "./assets/img/media/0.76-.jpg";
-import _8 from "./assets/img/media/0.77-.jpg";
-import _9 from "./assets/img/media/0.78-.jpg";
-import _10 from "./assets/img/media/0.79-.jpg";
-import _11 from "./assets/img/media/0.80-.jpg";
-import _12 from "./assets/img/media/0.81-.jpg";
-import _13 from "./assets/img/media/0.82-.jpeg";
-import _14 from "./assets/img/media/0.83-.jpeg";
-import _15 from "./assets/img/media/0.84-.jpeg";
-import _16 from "./assets/img/media/0.85-.jpeg";
-import _17 from "./assets/img/media/0.86-.jpeg";
-import _18 from "./assets/img/media/0.87-.jpeg";
-import _19 from "./assets/img/media/0.88-.jpeg";
-import _20 from "./assets/img/media/0.89-.jpeg";
-import _21 from "./assets/img/media/0.90-.jpeg";
-import _22 from "./assets/img/media/0.91-.jpeg";
-import _23 from "./assets/img/media/0.92-.jpeg";
-import _24 from "./assets/img/media/0.93-.jpeg";
-import _25 from "./assets/img/media/0.94-.jpeg";
-import _26 from "./assets/img/media/0.95-.jpeg";
-import _27 from "./assets/img/media/0.96-.jpeg";
-import _28 from "./assets/img/media/0.97-.jpeg";
-import _29 from "./assets/img/media/0.98-.jpeg";
-import _30 from "./assets/img/media/0.99-.jpeg";
-import _31 from "./assets/img/media/1-profil-marion.jpeg";
-import _32 from "./assets/img/media/2-bio-portrait.jpeg";
-import _33 from "./assets/img/media/2.5-bio-portrait.jpeg";
-import _34 from "./assets/img/media/3-contact.jpeg";
-import _35 from "./assets/img/media/4-hero.jpeg";
-import _36 from "./assets/img/media/5-bio-banner.jpeg";
-import _37 from "./assets/img/media/6-marion-nabil.jpeg";
-import _38 from "./assets/img/media/7-marion-soliste.jpeg";
-import _39 from "./assets/img/media/8-news.jpeg";
-import _40 from "./assets/img/media/9-bonnelie.jpeg";
-import _41 from "./assets/img/media/9.1-bonnelie.jpeg";
-import _42 from "./assets/img/media/10-bonnelie.jpeg";
-import _43 from "./assets/img/media/11-.jpeg";
-import _44 from "./assets/img/media/12-.jpeg";
-import _45 from "./assets/img/media/13-.jpeg";
-import _46 from "./assets/img/media/14-.jpeg";
-import _47 from "./assets/img/media/15-.jpeg";
-import _48 from "./assets/img/media/16-.jpeg";
-import _49 from "./assets/img/media/17-.jpeg";
-import _50 from "./assets/img/media/18-.jpeg";
-import _51 from "./assets/img/media/19-.jpeg";
-import _52 from "./assets/img/media/20-.jpeg";
+import _0 from "./assets/img/media/0.58.jpeg";
+import _1 from "./assets/img/media/0.59.jpeg";
+import _2 from "./assets/img/media/0.60.jpeg";
+import _3 from "./assets/img/media/0.61.jpeg";
+import _4 from "./assets/img/media/0.62.jpeg";
+import _5 from "./assets/img/media/0.63.jpeg";
+import _6 from "./assets/img/media/0.64.jpeg";
+import _7 from "./assets/img/media/0.65.jpeg";
+import _8 from "./assets/img/media/0.66.jpeg";
+import _9 from "./assets/img/media/0.67.jpeg";
+import _10 from "./assets/img/media/0.68.jpeg";
+import _11 from "./assets/img/media/0.69-.jpg";
+import _12 from "./assets/img/media/0.70-.jpg";
+import _13 from "./assets/img/media/0.71-.jpg";
+import _14 from "./assets/img/media/0.72-.jpg";
+import _15 from "./assets/img/media/0.73-.jpg";
+import _16 from "./assets/img/media/0.74-.jpg";
+import _17 from "./assets/img/media/0.75-.jpg";
+import _18 from "./assets/img/media/0.76-.jpg";
+import _19 from "./assets/img/media/0.77-.jpg";
+import _20 from "./assets/img/media/0.78-.jpg";
+import _21 from "./assets/img/media/0.79-.jpg";
+import _22 from "./assets/img/media/0.80-.jpg";
+import _23 from "./assets/img/media/0.81-.jpg";
+import _24 from "./assets/img/media/0.82-.jpeg";
+import _25 from "./assets/img/media/0.83-.jpeg";
+import _26 from "./assets/img/media/0.84-.jpeg";
+import _27 from "./assets/img/media/0.85-.jpeg";
+import _28 from "./assets/img/media/0.86-.jpeg";
+import _29 from "./assets/img/media/0.87-.jpeg";
+import _30 from "./assets/img/media/0.88-.jpeg";
+import _31 from "./assets/img/media/0.89-.jpeg";
+import _32 from "./assets/img/media/0.90-.jpeg";
+import _33 from "./assets/img/media/0.91-.jpeg";
+import _34 from "./assets/img/media/0.92-.jpeg";
+import _35 from "./assets/img/media/0.93-.jpeg";
+import _36 from "./assets/img/media/0.94-.jpeg";
+import _37 from "./assets/img/media/0.95-.jpeg";
+import _38 from "./assets/img/media/0.96-.jpeg";
+import _39 from "./assets/img/media/0.97-.jpeg";
+import _40 from "./assets/img/media/0.98-.jpeg";
+import _41 from "./assets/img/media/0.99-.jpeg";
+import _42 from "./assets/img/media/1-profil-marion.jpeg";
+import _43 from "./assets/img/media/2-bio-portrait.jpeg";
+import _44 from "./assets/img/media/2.5-bio-portrait.jpeg";
+import _45 from "./assets/img/media/3-contact.jpeg";
+import _46 from "./assets/img/media/4-hero.jpeg";
+import _47 from "./assets/img/media/5-bio-banner.jpeg";
+import _48 from "./assets/img/media/6-marion-nabil.jpeg";
+import _49 from "./assets/img/media/7-marion-soliste.jpeg";
+import _50 from "./assets/img/media/8-news.jpeg";
+import _51 from "./assets/img/media/9-bonnelie.jpeg";
+import _52 from "./assets/img/media/9.1-bonnelie.jpeg";
+import _53 from "./assets/img/media/10-bonnelie.jpeg";
+import _54 from "./assets/img/media/11-.jpeg";
+import _55 from "./assets/img/media/12-.jpeg";
+import _56 from "./assets/img/media/13-.jpeg";
+import _57 from "./assets/img/media/14-.jpeg";
+import _58 from "./assets/img/media/15-.jpeg";
+import _59 from "./assets/img/media/16-.jpeg";
+import _60 from "./assets/img/media/17-.jpeg";
+import _61 from "./assets/img/media/18-.jpeg";
+import _62 from "./assets/img/media/19-.jpeg";
+import _63 from "./assets/img/media/20-.jpeg";
 
 
 export const files = {
 "files": [
 {
 					"url": _0,
-					"name": "0.69-"
+					"name": "0.58"
 				},
 {
 					"url": _1,
-					"name": "0.70-"
+					"name": "0.59"
 				},
 {
 					"url": _2,
-					"name": "0.71-"
+					"name": "0.60"
 				},
 {
 					"url": _3,
-					"name": "0.72-"
+					"name": "0.61"
 				},
 {
 					"url": _4,
-					"name": "0.73-"
+					"name": "0.62"
 				},
 {
 					"url": _5,
-					"name": "0.74-"
+					"name": "0.63"
 				},
 {
 					"url": _6,
-					"name": "0.75-"
+					"name": "0.64"
 				},
 {
 					"url": _7,
-					"name": "0.76-"
+					"name": "0.65"
 				},
 {
 					"url": _8,
-					"name": "0.77-"
+					"name": "0.66"
 				},
 {
 					"url": _9,
-					"name": "0.78-"
+					"name": "0.67"
 				},
 {
 					"url": _10,
-					"name": "0.79-"
+					"name": "0.68"
 				},
 {
 					"url": _11,
-					"name": "0.80-"
+					"name": "0.69-"
 				},
 {
 					"url": _12,
-					"name": "0.81-"
+					"name": "0.70-"
 				},
 {
 					"url": _13,
-					"name": "0.82-"
+					"name": "0.71-"
 				},
 {
 					"url": _14,
-					"name": "0.83-"
+					"name": "0.72-"
 				},
 {
 					"url": _15,
-					"name": "0.84-"
+					"name": "0.73-"
 				},
 {
 					"url": _16,
-					"name": "0.85-"
+					"name": "0.74-"
 				},
 {
 					"url": _17,
-					"name": "0.86-"
+					"name": "0.75-"
 				},
 {
 					"url": _18,
-					"name": "0.87-"
+					"name": "0.76-"
 				},
 {
 					"url": _19,
-					"name": "0.88-"
+					"name": "0.77-"
 				},
 {
 					"url": _20,
-					"name": "0.89-"
+					"name": "0.78-"
 				},
 {
 					"url": _21,
-					"name": "0.90-"
+					"name": "0.79-"
 				},
 {
 					"url": _22,
-					"name": "0.91-"
+					"name": "0.80-"
 				},
 {
 					"url": _23,
-					"name": "0.92-"
+					"name": "0.81-"
 				},
 {
 					"url": _24,
-					"name": "0.93-"
+					"name": "0.82-"
 				},
 {
 					"url": _25,
-					"name": "0.94-"
+					"name": "0.83-"
 				},
 {
 					"url": _26,
-					"name": "0.95-"
+					"name": "0.84-"
 				},
 {
 					"url": _27,
-					"name": "0.96-"
+					"name": "0.85-"
 				},
 {
 					"url": _28,
-					"name": "0.97-"
+					"name": "0.86-"
 				},
 {
 					"url": _29,
-					"name": "0.98-"
+					"name": "0.87-"
 				},
 {
 					"url": _30,
-					"name": "0.99-"
+					"name": "0.88-"
 				},
 {
 					"url": _31,
-					"name": "profil-marion"
+					"name": "0.89-"
 				},
 {
 					"url": _32,
-					"name": "bio-portrait"
+					"name": "0.90-"
 				},
 {
 					"url": _33,
-					"name": "2.5-bio-portrait"
+					"name": "0.91-"
 				},
 {
 					"url": _34,
-					"name": "contact"
+					"name": "0.92-"
 				},
 {
 					"url": _35,
-					"name": "hero"
+					"name": "0.93-"
 				},
 {
 					"url": _36,
-					"name": "bio-banner"
+					"name": "0.94-"
 				},
 {
 					"url": _37,
-					"name": "marion-nabil"
+					"name": "0.95-"
 				},
 {
 					"url": _38,
-					"name": "marion-soliste"
+					"name": "0.96-"
 				},
 {
 					"url": _39,
-					"name": "news"
+					"name": "0.97-"
 				},
 {
 					"url": _40,
-					"name": "bonnelie"
+					"name": "0.98-"
 				},
 {
 					"url": _41,
-					"name": "9.1-bonnelie"
+					"name": "0.99-"
 				},
 {
 					"url": _42,
-					"name": "bonnelie"
+					"name": "profil-marion"
 				},
 {
 					"url": _43,
-					"name": ""
+					"name": "bio-portrait"
 				},
 {
 					"url": _44,
-					"name": ""
+					"name": "2.5-bio-portrait"
 				},
 {
 					"url": _45,
-					"name": ""
+					"name": "contact"
 				},
 {
 					"url": _46,
-					"name": ""
+					"name": "hero"
 				},
 {
 					"url": _47,
-					"name": ""
+					"name": "bio-banner"
 				},
 {
 					"url": _48,
-					"name": ""
+					"name": "marion-nabil"
 				},
 {
 					"url": _49,
-					"name": ""
+					"name": "marion-soliste"
 				},
 {
 					"url": _50,
-					"name": ""
+					"name": "news"
 				},
 {
 					"url": _51,
-					"name": ""
+					"name": "bonnelie"
 				},
 {
 					"url": _52,
+					"name": "9.1-bonnelie"
+				},
+{
+					"url": _53,
+					"name": "bonnelie"
+				},
+{
+					"url": _54,
+					"name": ""
+				},
+{
+					"url": _55,
+					"name": ""
+				},
+{
+					"url": _56,
+					"name": ""
+				},
+{
+					"url": _57,
+					"name": ""
+				},
+{
+					"url": _58,
+					"name": ""
+				},
+{
+					"url": _59,
+					"name": ""
+				},
+{
+					"url": _60,
+					"name": ""
+				},
+{
+					"url": _61,
+					"name": ""
+				},
+{
+					"url": _62,
+					"name": ""
+				},
+{
+					"url": _63,
 					"name": ""
 				},
 
--- src/generatedWebpExports.ts
+++ src/generatedWebpExports.ts
@@ -1,270 +1,325 @@
-import _0 from "./assets/webp/media/0.69-.webp";
-import _1 from "./assets/webp/media/0.70-.webp";
-import _2 from "./assets/webp/media/0.71-.webp";
-import _3 from "./assets/webp/media/0.72-.webp";
-import _4 from "./assets/webp/media/0.73-.webp";
-import _5 from "./assets/webp/media/0.74-.webp";
-import _6 from "./assets/webp/media/0.75-.webp";
-import _7 from "./assets/webp/media/0.76-.webp";
-import _8 from "./assets/webp/media/0.77-.webp";
-import _9 from "./assets/webp/media/0.78-.webp";
-import _10 from "./assets/webp/media/0.79-.webp";
-import _11 from "./assets/webp/media/0.80-.webp";
-import _12 from "./assets/webp/media/0.81-.webp";
-import _13 from "./assets/webp/media/0.82-.webp";
-import _14 from "./assets/webp/media/0.83-.webp";
-import _15 from "./assets/webp/media/0.84-.webp";
-import _16 from "./assets/webp/media/0.85-.webp";
-import _17 from "./assets/webp/media/0.86-.webp";
-import _18 from "./assets/webp/media/0.87-.webp";
-import _19 from "./assets/webp/media/0.88-.webp";
-import _20 from "./assets/webp/media/0.89-.webp";
-import _21 from "./assets/webp/media/0.90-.webp";
-import _22 from "./assets/webp/media/0.91-.webp";
-import _23 from "./assets/webp/media/0.92-.webp";
-import _24 from "./assets/webp/media/0.93-.webp";
-import _25 from "./assets/webp/media/0.94-.webp";
-import _26 from "./assets/webp/media/0.95-.webp";
-import _27 from "./assets/webp/media/0.96-.webp";
-import _28 from "./assets/webp/media/0.97-.webp";
-import _29 from "./assets/webp/media/0.98-.webp";
-import _30 from "./assets/webp/media/0.99-.webp";
-import _31 from "./assets/webp/media/1-profil-marion.webp";
-import _32 from "./assets/webp/media/2-bio-portrait.webp";
-import _33 from "./assets/webp/media/2.5-bio-portrait.webp";
-import _34 from "./assets/webp/media/3-contact.webp";
-import _35 from "./assets/webp/media/4-hero.webp";
-import _36 from "./assets/webp/media/5-bio-banner.webp";
-import _37 from "./assets/webp/media/6-marion-nabil.webp";
-import _38 from "./assets/webp/media/7-marion-soliste.webp";
-import _39 from "./assets/webp/media/8-news.webp";
-import _40 from "./assets/webp/media/9-bonnelie.webp";
-import _41 from "./assets/webp/media/9.1-bonnelie.webp";
-import _42 from "./assets/webp/media/10-bonnelie.webp";
-import _43 from "./assets/webp/media/11-.webp";
-import _44 from "./assets/webp/media/12-.webp";
-import _45 from "./assets/webp/media/13-.webp";
-import _46 from "./assets/webp/media/14-.webp";
-import _47 from "./assets/webp/media/15-.webp";
-import _48 from "./assets/webp/media/16-.webp";
-import _49 from "./assets/webp/media/17-.webp";
-import _50 from "./assets/webp/media/18-.webp";
-import _51 from "./assets/webp/media/19-.webp";
-import _52 from "./assets/webp/media/20-.webp";
+import _0 from "./assets/webp/media/0.58.webp";
+import _1 from "./assets/webp/media/0.59.webp";
+import _2 from "./assets/webp/media/0.60.webp";
+import _3 from "./assets/webp/media/0.61.webp";
+import _4 from "./assets/webp/media/0.62.webp";
+import _5 from "./assets/webp/media/0.63.webp";
+import _6 from "./assets/webp/media/0.64.webp";
+import _7 from "./assets/webp/media/0.65.webp";
+import _8 from "./assets/webp/media/0.66.webp";
+import _9 from "./assets/webp/media/0.67.webp";
+import _10 from "./assets/webp/media/0.68.webp";
+import _11 from "./assets/webp/media/0.69-.webp";
+import _12 from "./assets/webp/media/0.70-.webp";
+import _13 from "./assets/webp/media/0.71-.webp";
+import _14 from "./assets/webp/media/0.72-.webp";
+import _15 from "./assets/webp/media/0.73-.webp";
+import _16 from "./assets/webp/media/0.74-.webp";
+import _17 from "./assets/webp/media/0.75-.webp";
+import _18 from "./assets/webp/media/0.76-.webp";
+import _19 from "./assets/webp/media/0.77-.webp";
+import _20 from "./assets/webp/media/0.78-.webp";
+import _21 from "./assets/webp/media/0.79-.webp";
+import _22 from "./assets/webp/media/0.80-.webp";
+import _23 from "./assets/webp/media/0.81-.webp";
+import _24 from "./assets/webp/media/0.82-.webp";
+import _25 from "./assets/webp/media/0.83-.webp";
+import _26 from "./assets/webp/media/0.84-.webp";
+import _27 from "./assets/webp/media/0.85-.webp";
+import _28 from "./assets/webp/media/0.86-.webp";
+import _29 from "./assets/webp/media/0.87-.webp";
+import _30 from "./assets/webp/media/0.88-.webp";
+import _31 from "./assets/webp/media/0.89-.webp";
+import _32 from "./assets/webp/media/0.90-.webp";
+import _33 from "./assets/webp/media/0.91-.webp";
+import _34 from "./assets/webp/media/0.92-.webp";
+import _35 from "./assets/webp/media/0.93-.webp";
+import _36 from "./assets/webp/media/0.94-.webp";
+import _37 from "./assets/webp/media/0.95-.webp";
+import _38 from "./assets/webp/media/0.96-.webp";
+import _39 from "./assets/webp/media/0.97-.webp";
+import _40 from "./assets/webp/media/0.98-.webp";
+import _41 from "./assets/webp/media/0.99-.webp";
+import _42 from "./assets/webp/media/1-profil-marion.webp";
+import _43 from "./assets/webp/media/2-bio-portrait.webp";
+import _44 from "./assets/webp/media/2.5-bio-portrait.webp";
+import _45 from "./assets/webp/media/3-contact.webp";
+import _46 from "./assets/webp/media/4-hero.webp";
+import _47 from "./assets/webp/media/5-bio-banner.webp";
+import _48 from "./assets/webp/media/6-marion-nabil.webp";
+import _49 from "./assets/webp/media/7-marion-soliste.webp";
+import _50 from "./assets/webp/media/8-news.webp";
+import _51 from "./assets/webp/media/9-bonnelie.webp";
+import _52 from "./assets/webp/media/9.1-bonnelie.webp";
+import _53 from "./assets/webp/media/10-bonnelie.webp";
+import _54 from "./assets/webp/media/11-.webp";
+import _55 from "./assets/webp/media/12-.webp";
+import _56 from "./assets/webp/media/13-.webp";
+import _57 from "./assets/webp/media/14-.webp";
+import _58 from "./assets/webp/media/15-.webp";
+import _59 from "./assets/webp/media/16-.webp";
+import _60 from "./assets/webp/media/17-.webp";
+import _61 from "./assets/webp/media/18-.webp";
+import _62 from "./assets/webp/media/19-.webp";
+import _63 from "./assets/webp/media/20-.webp";
 
 
 export const files = {
 "files": [
 {
 					"url": _0,
-					"name": "0.69-"
+					"name": "0.58"
 				},
 {
 					"url": _1,
-					"name": "0.70-"
+					"name": "0.59"
 				},
 {
 					"url": _2,
-					"name": "0.71-"
+					"name": "0.60"
 				},
 {
 					"url": _3,
-					"name": "0.72-"
+					"name": "0.61"
 				},
 {
 					"url": _4,
-					"name": "0.73-"
+					"name": "0.62"
 				},
 {
 					"url": _5,
-					"name": "0.74-"
+					"name": "0.63"
 				},
 {
 					"url": _6,
-					"name": "0.75-"
+					"name": "0.64"
 				},
 {
 					"url": _7,
-					"name": "0.76-"
+					"name": "0.65"
 				},
 {
 					"url": _8,
-					"name": "0.77-"
+					"name": "0.66"
 				},
 {
 					"url": _9,
-					"name": "0.78-"
+					"name": "0.67"
 				},
 {
 					"url": _10,
-					"name": "0.79-"
+					"name": "0.68"
 				},
 {
 					"url": _11,
-					"name": "0.80-"
+					"name": "0.69-"
 				},
 {
 					"url": _12,
-					"name": "0.81-"
+					"name": "0.70-"
 				},
 {
 					"url": _13,
-					"name": "0.82-"
+					"name": "0.71-"
 				},
 {
 					"url": _14,
-					"name": "0.83-"
+					"name": "0.72-"
 				},
 {
 					"url": _15,
-					"name": "0.84-"
+					"name": "0.73-"
 				},
 {
 					"url": _16,
-					"name": "0.85-"
+					"name": "0.74-"
 				},
 {
 					"url": _17,
-					"name": "0.86-"
+					"name": "0.75-"
 				},
 {
 					"url": _18,
-					"name": "0.87-"
+					"name": "0.76-"
 				},
 {
 					"url": _19,
-					"name": "0.88-"
+					"name": "0.77-"
 				},
 {
 					"url": _20,
-					"name": "0.89-"
+					"name": "0.78-"
 				},
 {
 					"url": _21,
-					"name": "0.90-"
+					"name": "0.79-"
 				},
 {
 					"url": _22,
-					"name": "0.91-"
+					"name": "0.80-"
 				},
 {
 					"url": _23,
-					"name": "0.92-"
+					"name": "0.81-"
 				},
 {
 					"url": _24,
-					"name": "0.93-"
+					"name": "0.82-"
 				},
 {
 					"url": _25,
-					"name": "0.94-"
+					"name": "0.83-"
 				},
 {
 					"url": _26,
-					"name": "0.95-"
+					"name": "0.84-"
 				},
 {
 					"url": _27,
-					"name": "0.96-"
+					"name": "0.85-"
 				},
 {
 					"url": _28,
-					"name": "0.97-"
+					"name": "0.86-"
 				},
 {
 					"url": _29,
-					"name": "0.98-"
+					"name": "0.87-"
 				},
 {
 					"url": _30,
-					"name": "0.99-"
+					"name": "0.88-"
 				},
 {
 					"url": _31,
-					"name": "profil-marion"
+					"name": "0.89-"
 				},
 {
 					"url": _32,
-					"name": "bio-portrait"
+					"name": "0.90-"
 				},
 {
 					"url": _33,
-					"name": "2.5-bio-portrait"
+					"name": "0.91-"
 				},
 {
 					"url": _34,
-					"name": "contact"
+					"name": "0.92-"
 				},
 {
 					"url": _35,
-					"name": "hero"
+					"name": "0.93-"
 				},
 {
 					"url": _36,
-					"name": "bio-banner"
+					"name": "0.94-"
 				},
 {
 					"url": _37,
-					"name": "marion-nabil"
+					"name": "0.95-"
 				},
 {
 					"url": _38,
-					"name": "marion-soliste"
+					"name": "0.96-"
 				},
 {
 					"url": _39,
-					"name": "news"
+					"name": "0.97-"
 				},
 {
 					"url": _40,
-					"name": "bonnelie"
+					"name": "0.98-"
 				},
 {
 					"url": _41,
-					"name": "9.1-bonnelie"
+					"name": "0.99-"
 				},
 {
 					"url": _42,
-					"name": "bonnelie"
+					"name": "profil-marion"
 				},
 {
 					"url": _43,
-					"name": ""
+					"name": "bio-portrait"
 				},
 {
 					"url": _44,
-					"name": ""
+					"name": "2.5-bio-portrait"
 				},
 {
 					"url": _45,
-					"name": ""
+					"name": "contact"
 				},
 {
 					"url": _46,
-					"name": ""
+					"name": "hero"
 				},
 {
 					"url": _47,
-					"name": ""
+					"name": "bio-banner"
 				},
 {
 					"url": _48,
-					"name": ""
+					"name": "marion-nabil"
 				},
 {
 					"url": _49,
-					"name": ""
+					"name": "marion-soliste"
 				},
 {
 					"url": _50,
-					"name": ""
+					"name": "news"
 				},
 {
 					"url": _51,
-					"name": ""
+					"name": "bonnelie"
 				},
 {
 					"url": _52,
+					"name": "9.1-bonnelie"
+				},
+{
+					"url": _53,
+					"name": "bonnelie"
+				},
+{
+					"url": _54,
+					"name": ""
+				},
+{
+					"url": _55,
+					"name": ""
+				},
+{
+					"url": _56,
+					"name": ""
+				},
+{
+					"url": _57,
+					"name": ""
+				},
+{
+					"url": _58,
+					"name": ""
+				},
+{
+					"url": _59,
+					"name": ""
+				},
+{
+					"url": _60,
+					"name": ""
+				},
+{
+					"url": _61,
+					"name": ""
+				},
+{
+					"url": _62,
+					"name": ""
+				},
+{
+					"url": _63,
 					"name": ""
 				},
 
